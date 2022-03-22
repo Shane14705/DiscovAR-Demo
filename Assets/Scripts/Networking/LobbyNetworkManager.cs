45667b8b7855d0ca79df473c7dac7aa7c85cdb95
Shane McKelvey <psmnhb@umsystem.edu>
2022-03-21 20:56:21 -0500

    Cleaned up UI callbacks
    Realized I did not need a button for creating a room, as photon will create a room if a user tries to join a room that does not yet exist.

--- Assets/Scripts/Networking/LobbyNetworkManager.cs
+++ Assets/Scripts/Networking/LobbyNetworkManager.cs
@@ -1,18 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyNetworkManager : MonoBehaviour
 {
+    [SerializeField] private InputField _enteredRoomName;
+    private RoomOptions _roomOptions;
 
-    public void TryCreateRoom()
+    private void OnEnable()
     {
-        throw new NotImplementedException();
+        _roomOptions.MaxPlayers = 0;
+        //Room will last up to a minute after everyone has left
+        _roomOptions.EmptyRoomTtl = 60000;
     }
 
     public void TryJoinRoom()
     {
-        throw new NotImplementedException();
+        if (_enteredRoomName.text != null)
+        {
+            PhotonNetwork.JoinOrCreateRoom(_enteredRoomName.text, _roomOptions, TypedLobby.Default);
+            throw new NotImplementedException();
+        }
     }
 }

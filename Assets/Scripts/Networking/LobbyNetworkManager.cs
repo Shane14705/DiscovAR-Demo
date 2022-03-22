364909fda64d4551ec7faec635c7753e07dae2f5
Shane McKelvey <psmnhb@umsystem.edu>
2022-03-22 12:35:06 -0500

    Got a connection to Photon Servers
    Boilerplate room loading logic works, time to get to work on the viewing scenes

--- Assets/Scripts/Networking/LobbyNetworkManager.cs
+++ Assets/Scripts/Networking/LobbyNetworkManager.cs
@@ -6,24 +6,51 @@ using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LobbyNetworkManager : MonoBehaviour
+public class LobbyNetworkManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private InputField _enteredRoomName;
-    private RoomOptions _roomOptions;
+    
 
-    private void OnEnable()
+    public override void OnCreateRoomFailed(short returnCode, string message)
     {
-        _roomOptions.MaxPlayers = 0;
-        //Room will last up to a minute after everyone has left
-        _roomOptions.EmptyRoomTtl = 60000;
+        base.OnCreateRoomFailed(returnCode, message);
+        Debug.Log("Couldnt create room: " + message);
     }
 
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Successfully joined room: " + PhotonNetwork.CurrentRoom);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Connected!");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        base.OnJoinRoomFailed(returnCode, message);
+        Debug.Log("Failed to join room: " + message);
+    }
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    
+    
     public void TryJoinRoom()
     {
         if (_enteredRoomName.text != null)
         {
+            RoomOptions _roomOptions = new RoomOptions();
+            _roomOptions.MaxPlayers = 0;
+            //Room will last up to a minute after everyone has left
+            _roomOptions.EmptyRoomTtl = 60000;
             PhotonNetwork.JoinOrCreateRoom(_enteredRoomName.text, _roomOptions, TypedLobby.Default);
-            throw new NotImplementedException();
         }
     }
 }

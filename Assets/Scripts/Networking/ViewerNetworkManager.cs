d565a2ff29a73d48cbdc5d7360482626a8c8a9df
Shane McKelvey <psmnhb@umsystem.edu>
2022-03-23 13:44:59 -0500

    Start architecting viewer scripting
    Idea: the input controller for modifying the object is included *on* the object prefab, however the network manager is always existent even when a model isn't selected. Model Input controller contacts viewer network manager when it needs to network something (eg: Add an annotation). There will be an additional script on the canvas just to handle UI stuff (model picking, exit) that also connects to network manager.

--- Assets/Scripts/Networking/ViewerNetworkManager.cs
+++ Assets/Scripts/Networking/ViewerNetworkManager.cs
@@ -1,18 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ViewerNetworkManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //We need to make sure that we are connected to the Photon Room, if we aren't then we need to handle it somehow...
+        throw new NotImplementedException();
     }
 }

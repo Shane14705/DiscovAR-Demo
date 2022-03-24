16ef17f1a00326457b740b963fd8fe35a6b17935
Shane McKelvey <psmnhb@umsystem.edu>
2022-03-23 19:19:29 -0500

    Finished some prototype UI and added scripts

--- Assets/Scripts/Networking/ViewerNetworkManager.cs
+++ Assets/Scripts/Networking/ViewerNetworkManager.cs
@@ -1,13 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
-public class ViewerNetworkManager : MonoBehaviour
+public class ViewerNetworkManager : MonoBehaviourPunCallbacks
 {
+    
     private void Start()
     {
         //We need to make sure that we are connected to the Photon Room, if we aren't then we need to handle it somehow...
+        if (!PhotonNetwork.InRoom)
+        {
+            Debug.Log("ERROR: Made it into viewer scene without being connected to a room!");
+        }
         throw new NotImplementedException();
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerLeftRoom(otherPlayer);
     }
 }

24dd99dbcb7f38efddff88e9adba9b68ba0e0270
Shane McKelvey <psmnhb@umsystem.edu>
2022-04-09 14:37:49 -0500

    Fixed scale of saturn model to be more in line with other models
    this will make it so i can use the same size annotation colliders on every model

---
+++ Assets/Scripts/Annotate/AnnotationController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This should go on the spawned annotation prefab, itll contain the code for hiding/showing (creating) the annotation UI, and updating the annotation info
+//Ex: onselected method to toggle ui when inputHandler detects that this annotation has been clicked
+public class AnnotationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

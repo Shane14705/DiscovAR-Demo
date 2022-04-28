495d9e026cadd70ad1fe6f871dc6dfb1e80b5494
Shane McKelvey <psmnhb@umsystem.edu>
2022-04-27 19:47:56 -0500

    Began working on AR viewer

--- Assets/Scripts/Networking/ViewerNetworkManager.cs
+++ Assets/Scripts/Networking/ViewerNetworkManager.cs
@@ -4,11 +4,31 @@ using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
 
 public class ViewerNetworkManager : MonoBehaviourPunCallbacks
 {
     private PhotonView _photonView;
     private GameObject _currentModel = null;
+
+    [SerializeField] private bool _arScene = false;
+    [SerializeField] private ARRaycastManager _arRaycastManager;
+
+    
+    public ARRaycastManager ARRaycastManager
+    {
+        get
+        {
+            if (_arScene) return _arRaycastManager;
+            else
+            {
+                Debug.Log("ERROR: trying to get ARRaycastManager in non-ar scene!");
+                return _arRaycastManager;
+            }
+        }
+        set => _arRaycastManager = value;
+    }
 
     public GameObject CurrentModel
     {
@@ -24,6 +44,13 @@ public class ViewerNetworkManager : MonoBehaviourPunCallbacks
         {
             Debug.Log("ERROR: Made it into viewer scene without being connected to a room!");
         }
+
+        if (_arScene)
+        {
+            
+            Debug.Log("Tap a surface to pick your object spawn location!");
+        }
+        
     }
     
     public override void OnLeftRoom()
